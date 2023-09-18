e0fdeabe32be2584262acd4b1f058310ae4ac9bb
Jakob Douglas <jakobdouglas1@gmail.com>
2023-09-18 15:13:21 +0930

    chore: set up styled compopnents and created some reusable style components for the app aswell as some 'psuedo tailwind classes'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import GlobalStyles from './styles/GlobalStyles';
+
+import Button from './ui/Button';
+import Heading from './ui/Heading';
+import { Input } from './ui/Input';
+import styled from 'styled-components';
+import Row from './ui/Row';
+
+const StyledApp = styled.main`
+	/* background-color: orangered; */
+	padding: 20px;
+`;
+
 function App() {
-	return <div>hey</div>;
+	return (
+		<>
+			<GlobalStyles />
+			<StyledApp>
+				<Row>
+					<Row type='horizontal'>
+						<Heading as='h1'>The Wild Oasis</Heading>
+						<div>
+							<Heading as='h2'>Check in and out</Heading>
+							<Button onClick={() => alert('check in')}> Check in</Button>
+							<Button
+								variation='secondary'
+								size='small'
+								onClick={() => alert('check out')}
+							>
+								{' '}
+								Check out
+							</Button>
+						</div>
+					</Row>
+					<Row>
+						<Heading as='h3'>Form</Heading>
+						<form>
+							<Input type='number' placeholder='Number of guests' />
+							<Input type='number' placeholder='Number of guests' />
+						</form>
+					</Row>
+				</Row>
+			</StyledApp>
+		</>
+	);
 }
 
 export default App;
