ed89a8a33bfd5f6fcac69ffe00d0dd69bcf797d7
Jakob Douglas <jakobdouglas1@gmail.com>
2023-09-20 15:08:35 +0930

    feat: set up supabase backend for project and linked it to front end, still in teasting phase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,32 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect, useState } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabins';
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+	const [cabins, setCabins] = useState('');
+
+	useEffect(
+		() =>
+			async function data() {
+				const res = await getCabins();
+				setCabins(res);
+				console.log(res);
+			},
+
+		[]
+	);
+
+	return (
+		<Row type='horizontal'>
+			<Heading as='h1'>all cabins</Heading>
+			<img
+				src='https://ynjqqejuebcocufsdsnr.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg'
+				alt='cabin-0'
+			/>
+			<p>TEST</p>
+		</Row>
+	);
 }
 
 export default Cabins;
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ynjqqejuebcocufsdsnr.supabase.co';
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InluanFxZWp1ZWJjb2N1ZnNkc25yIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUxNzY1NDEsImV4cCI6MjAxMDc1MjU0MX0.a6bHhZzFa24U7el0-B2PRuI9WXD0rp_pjpmVoyCYvf8';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
