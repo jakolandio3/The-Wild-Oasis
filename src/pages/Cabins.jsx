45e662ad665a12f93dfed3e6b1be0379e56dda9f
Jakob Douglas <jakobdouglas1@gmail.com>
2023-09-21 09:45:50 +0930

    chore: added backend row level security with supabase and currently allow some mulations with public key for testing purposes, also initialised react query for data fetching and remote state mutations

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,31 +1,20 @@
-import { useEffect, useState } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
+
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-	const [cabins, setCabins] = useState('');
-
-	useEffect(
-		() =>
-			async function data() {
-				const res = await getCabins();
-				setCabins(res);
-				console.log(res);
-			},
-
-		[]
-	);
-
 	return (
-		<Row type='horizontal'>
-			<Heading as='h1'>all cabins</Heading>
-			<img
-				src='https://ynjqqejuebcocufsdsnr.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg'
-				alt='cabin-0'
-			/>
-			<p>TEST</p>
-		</Row>
+		<>
+			<Row type='horizontal'>
+				<Heading as='h1'>all cabins</Heading>
+
+				<p>Filter / Sort</p>
+			</Row>
+			<Row>
+				<CabinTable />
+			</Row>
+		</>
 	);
 }
 
